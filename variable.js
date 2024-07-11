23bcdceb93439c500923e14423a1fa0f8a3e6829
Abhishek Thakur <abhishek.thakur@kapturecrm.com>
2024-07-11 15:46:18 +0530

    files

---
+++ variable.js
@@ -0,0 +1,23 @@
+// In this <commit-M> we are going to study about JavaScript Variables
+
+// Javascript Varibable: Variables are used to store data in JavaScript.
+//Variables are used to store reusable values.The values of the variables are allocated using the assignment operator(“=”). 
+// Variable can store the information
+// We can use that information later or chnage that information later
+
+// assignment operator: The assignment operator = assigns the value of its right-hand operand to a variable, a property, or an indexer element given by its left-hand operand. The result of an assignment expression is the value assigned to the left-hand operand.
+
+// variable name are case sensitive used them same as you defined in your code file other it will give your error
+
+// "use Strict";
+
+// declare variable
+// use variable
+// chnage Value
+
+var firstName = "Abhishek Thakur,Web-dev,JavaScript";
+console.log(firstName);
+firstName = "JavaScript New";
+console.log(firstName);
+
+
