fe5d07f6ac3e335cd7c0d07c44bdc91173824260
Abhishek Thakur <abhishek.thakur@kapturecrm.com>
2024-07-13 13:09:13 +0530

    others

--- undefinedNull.js
+++ undefinedNull.js
@@ -1,8 +1,26 @@
 // undefined
-// null
+
 let firstName;
 console.log(typeof firstName); // output is undefined
-
-//null
 let firstname = "";
 console.log(typeof firstname); //output is string
+
+// null
+let myVariable = null;
+console.log(myVariable);
+myVariable = "thakur"
+console.log(myVariable, typeof myVariable);
+console.log(typeof null);
+// bug  error in javascript
+
+
+//BigInt
+let myNumber = 123n;
+console.log(myNumber); 
+
+let myNumber2 = BigInt(2); //BigInt here is constructor
+console.log(myNumber2);
+console.log(myNumber2+ myNumber);
+// error undefinedNull.js:23 Uncaught TypeError: Cannot mix BigInt and other types, use explicit conversions at undefinedNull.js:23:22
+// you can mixed other number with BigInt
+// by adding n to myNumber it will give you output is 125n
