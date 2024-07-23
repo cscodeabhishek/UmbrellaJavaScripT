01141ec9c1892a7c4223a33a2c828a29d1719b61
Abhishek Thakur <abhishek.thakur@kapturecrm.com>
2024-07-23 15:50:57 +0530

    addingfiles

---
+++ dowhile21.js
@@ -0,0 +1,15 @@
+//while loop
+// normal while llop
+let i = 0;
+while (i <= 9) {
+    console.log(i);
+    i++;
+}
+// do while loop
+let j = 0;
+do {
+    console.log(i);
+    j++;
+} while (j <= 9);
+
+console.log("value of i ") 
---
+++ forloop.js
@@ -0,0 +1,21 @@
+// continue keyword
+for (let i = 1; i <= 10; i++){
+    if (i == 4) {
+        break; // it will print till 4 and then break; the statement even the condition is true as hello there and then terminate because of break;
+    }
+    console.log(i);
+}
+console.log("hellow there");
+
+
+
+// conitinue
+
+for (let i = 1; i <= 10; i++){
+    if (i == 4) {
+        continue;
+    }
+    console.log(i);
+}
+console.log("hellow there");
+// op 1 to 10 and hellow there
