a91b8978ae1e4f428fc07b26a2082b4d082d00b8
Abhishek Thakur <abhishek.thakur@kapturecrm.com>
2024-07-27 17:35:32 +0530

    files

--- array/constarray7.js
+++ array/constarray7.js
@@ -1,1 +1,13 @@
 // const array js
+const pi = 3.14;
+// pi = 12;
+console.log(pi);//Output error constarray7.js:3 Uncaught TypeError: Assignment to constant variable.
+
+const fruits = ["apple", "banana"];
+fruits = ["pineapple"];
+fruits.push("kiwi");
+console.log(fruits); // in array it got push in const var
+//this is happening because fruits array having address of 0x11 and stored in heap 
+//we are not changing this address[0x11]. we just push the new fruits
+// this address will be going to fetch when fruits = ["kiwi"], because in heap this need to store on some address and by rule it give you error as same as Above 
+// error: error constarray7 Uncaught TypeError: Assignment to constant variable.
---
+++ array/whilelooparray8.js
@@ -0,0 +1,6 @@
+const fruits = ["apple", "kiwi", "pineapple"];
+let i=0
+while (i<fruits.length) {
+    console.log(fruits[i]);
+}
+console.log(fruits); 
