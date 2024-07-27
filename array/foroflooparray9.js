b013052f0a4b7c788834fd8e9a0477a1d6ad9b1c
Abhishek Thakur <abhishek.thakur@kapturecrm.com>
2024-07-27 21:13:32 +0530

    files

---
+++ array/foroflooparray9.js
@@ -0,0 +1,13 @@
+//for of loop in array
+// Loop :Iterate
+const fruits = ["apple", "dragonfruit", "kiwi", "redberry"]
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// for in loop
+const fruitss = ["apple", "dragonfruit", "kiwi", "redberry"]
+const fruitsss = [];
+for (let index in fruitss) {
+    console.log(fruitss[index]);
+}
