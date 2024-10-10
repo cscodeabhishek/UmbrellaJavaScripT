af6648acae73f0fe57c18ff791999cdf2e5deb3e
your_name <dev.codescse@gmail.com>
2024-10-11 01:11:56 +0530

    newfilesAdded

--- array/arraypushpop3.js
+++ array/arraypushpop3.js
@@ -1,10 +1,10 @@
 // push pop
 let fruits = ['apple', 'orange', 'grapes', 'kiwi'];
 console.log(fruits);
-//push ---->in the last
-// fruits.push = ('banana');
-// console.log(fruits)
-//pop-->delete the element from the array from the last index
+push ---->in the last
+fruits.push = ('banana');
+console.log(fruits)
+pop-->delete the element from the array from the last index
 let poppedFruits = fruits.pop();
 console.log('Popped Fruits is',poppedFruits);
 console.log(fruits);
