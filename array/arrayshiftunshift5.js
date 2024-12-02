770e46c7d29f90ee2199bda712272af6fda83c05
Project-Management-Board <devcodes.cse@gmail.com>
2024-12-02 12:16:35 +0530

    initial updates

--- array/arrayshiftunshift5.js
+++ array/arrayshiftunshift5.js
@@ -2,9 +2,9 @@
 let fruits = ["apple", "banana", "orange", "watermelon"];
 console.log(fruits);
 // if you want to add the element from starting to the arrays used
-// fruits.unshift("kiwi");
-// console.log(fruits);
-// fruits.shift();
+fruits.unshift("kiwi");
+console.log(fruits);
+fruits.shift();
 // console.log(fruits);
 let removedFruits = fruits.shift(); // shift removed from starting element of an array
 console.log("removed fruits is", removedFruits)
